90c00f75ed646783c853fd0892faf85c8a46b3e4
Xue Qianming <qianmingxue@microsoft.com>
2022-04-22 15:29:11 +0800

    init

---
+++ ReverseProxy/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Proxy;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddProxy(
+    options =>
+        {
+            options.PrepareRequest = (originalRequest, message) =>
+            {
+                message.Headers.Add("X-Forwarded-Host", originalRequest.Host.Host);
+                return Task.FromResult(0);
+            };
+        });
+
+var app = builder.Build();
+
+app.UseWebSockets().RunProxy(new Uri(args[0]));
+
+app.Run();
---
+++ ReverseProxy/Startup.cs
@@ -0,0 +1,19 @@
+﻿public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddProxy(options =>
+        {
+            options.PrepareRequest = (originalRequest, message) =>
+            {
+                message.Headers.Add("X-Forwarded-Host", originalRequest.Host.Host);
+                return Task.FromResult(0);
+            };
+        });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseWebSockets().RunProxy(new Uri("https://example.com"));
+    }
+}
