478f010c519be7805b2f54ee07d41605d106005c
Xue Qianming <qianmingxue@microsoft.com>
2022-04-25 15:19:12 +0800

    remove ssl cert check

--- ReverseProxy/Program.cs
+++ ReverseProxy/Program.cs
@@ -5,8 +5,21 @@ using Microsoft.AspNetCore.Proxy;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 var builder = WebApplication.CreateBuilder(args);
+// disable ssl cert check
+ServicePointManager.ServerCertificateValidationCallback += 
+    delegate(
+        Object sender1,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors)
+    {
+        return true;
+    };
 builder.Services.AddProxy(
     options =>
         {
