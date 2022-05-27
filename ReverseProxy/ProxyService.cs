c347dda33d5539769384d476780d1b20d4ff9c59
Xue Qianming <39435859+SleepyBag@users.noreply.github.com>
2022-05-27 14:21:23 +0800

    Update ProxyService.cs

--- ReverseProxy/ProxyService.cs
+++ ReverseProxy/ProxyService.cs
@@ -18,16 +18,21 @@ namespace Microsoft.AspNetCore.Proxy
 
             Options = options.Value;
 
-            var handler = new HttpClientHandler { AllowAutoRedirect = false, UseCookies = false};
-            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
-            handler.ServerCertificateCustomValidationCallback = 
-                (httpRequestMessage, cert, cetChain, policyErrors) =>
-            {
-                return true;
+            var sslOptions = new SslClientAuthenticationOptions {
+                // Leave certs unvalidated for debugging
+                RemoteCertificateValidationCallback = delegate { return true; },
             };
 
-            Client = new HttpClient(Options.MessageHandler ?? handler);
-            Client.DefaultRequestVersion = new Version(2, 0);
+            var handler = new SocketsHttpHandler {
+                UseProxy = false,
+                AllowAutoRedirect = false,
+                AutomaticDecompression = DecompressionMethods.None,
+                UseCookies = false,
+                SslOptions = sslOptions,
+                EnableMultipleHttp2Connections = true,
+            };
+
+            Client = new HttpClient(Options.MessageHandler ?? handler, disposeHandler: true);
         }
 
         public SharedProxyOptions Options { get; private set; }
