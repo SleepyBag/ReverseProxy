46f7f431fbe96dd5653336f7f3f26482b1ff0887
Xue Qianming <qianmingxue@microsoft.com>
2022-06-21 14:26:24 +0800

    Remove useless using

--- ReverseProxy/Program.cs
+++ ReverseProxy/Program.cs
@@ -1,14 +1,3 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Proxy;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-using System.Net.Security;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddProxy(
